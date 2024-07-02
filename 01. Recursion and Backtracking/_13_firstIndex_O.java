979f47162e518ff56ff4024731383abefaace28e
shivam vishwakarma <arvind.972176@gmail.com>
2024-07-02 14:37:48 +0530

    first Index Optimized

---
+++ 01. Recursion and Backtracking/_13_firstIndex_O.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class _13_firstIndex_O {
+ public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        int x=sc.nextInt();
+        int ans=firstIndex(arr,0,x);
+        System.out.println(ans);
+        sc.close();
+
+    }
+
+    public static int firstIndex(int[]arr,int idx,int x){
+        if(idx==arr.length) return -1;// Base Case
+
+        // Faith
+        int val=firstIndex(arr, idx+1, x);
+
+        // Meeting faith with Expectation
+        if(arr[idx]==x) return idx;
+        // val==-1 or val
+        return val;
+    }
+}
