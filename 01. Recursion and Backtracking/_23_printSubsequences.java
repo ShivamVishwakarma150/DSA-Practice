eec74dcd499d9b3539ff8c945a9db02becc98fdd
shivam vishwakarma <arvind.972176@gmail.com>
2024-07-06 18:58:21 +0530

    printSubsequences

---
+++ 01. Recursion and Backtracking/_23_printSubsequences.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class _23_printSubsequences {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str = sc.next();
+        printSS(0, str,"");
+        sc.close();
+    }
+    public static void printSS(int idx, String str,String output) {
+        // // Base Case
+        if(idx==str.length()){
+            System.out.println(output);
+            return;
+        }
+        
+        char ch=str.charAt(idx);
+
+        //MFE
+        // Yes
+        printSS(idx+1, str, output+ch);
+        //No
+        printSS(idx+1, str, output);
+
+    }
+}
