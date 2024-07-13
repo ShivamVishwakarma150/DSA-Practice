c25ea3d73e81b086a622c0a54945d3c44e4ccde4
shivam vishwakarma <arvind.972176@gmail.com>
2024-07-13 18:52:17 +0530

    Combinations Leetcode 77

---
+++ 01. Recursion and Backtracking/_34_Combinations_Leetcode_77.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class _34_Combinations_Leetcode_77 {
+    public void combine(List<List<Integer>> combinations, List<Integer> combination,
+            int currentBox, int n, int k) {
+
+        if (currentBox == n) {
+            if (combination.size() == k) {
+                List<Integer> temp = new ArrayList<Integer>(combination);
+                combinations.add(temp);
+            }
+            return;
+        }
+
+        combination.add(currentBox+1);
+        combine(combinations, combination, currentBox + 1, n, k); // jab ham item le rhe hain
+        combination.remove(combination.size()-1);
+
+        combine(combinations, combination, currentBox + 1, n, k); // jab ham item nhi le rhe hain
+    }
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> combinations = new ArrayList<>();
+        List<Integer> combination = new ArrayList<>();
+        combine(combinations, combination, 0, n, k);
+
+        return combinations;
+    }
+}
