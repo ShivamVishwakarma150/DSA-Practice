9bc0498d6bc61062a581bfcc88cc78178a4e72f5
Shivam Vishwakarma <arvind.972176@gmail.com>
2024-10-02 19:25:10 +0530

    Data Members

---
+++ 02. Linked List/_01_Data_Members.java
@@ -0,0 +1,16 @@
+class Node{
+    int data;
+    Node next;
+}
+
+class Linkedlist{
+    Node head;
+    Node tail;
+    int size;
+}
+
+class _01_Data_Members {
+    public static void main(String[] args) {
+        
+    }
+}
